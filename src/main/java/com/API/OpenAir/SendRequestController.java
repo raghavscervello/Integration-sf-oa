670449639e8f5a806d5392f467e3d65c2979bc6b
Sharma <rsharm05@atkearney.com>
2021-05-03 15:34:11 +0530

    changing the JDK to 1.8

--- src/main/java/com/API/OpenAir/SendRequestController.java
+++ src/main/java/com/API/OpenAir/SendRequestController.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping; 
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
